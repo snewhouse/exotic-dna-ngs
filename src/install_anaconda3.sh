a45b4a27cde2e9e38f771b829e5bd8d4616d7ae6
snewhouse <stephen.j.newhouse@gmail.com>
2017-07-07 14:59:20 +0100

    sep conda install scripts: start

---
+++ src/install_anaconda3.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#!/usr/bin/env bash
+readonly ANCONDA_URL="https://repo.continuum.io/archive" ## 4.4.0
+readonly CONDA="Anaconda3"
+readonly CONDA_VERSION="4.4.0" # 4.4.0
+readonly ARCH="Linux-x86_64" # MacOSXx86_64
+INSTALL_DIR="${1-HOME}" # "${HOME}"
+echo "[INFO: $(date)] Installing: ${CONDA}-${CONDA_VERSION}-${ARCH}"
+echo "[INFO: $(date)] Installation Directory: ${INSTALL_DIR}"
+CONDA_BASH_INSTALL="${CONDA}-${CONDA_VERSION}-${ARCH}.sh"
+# install
+wget ${MINICONDA_URL}/${CONDA_BASH_INSTALL} -O ${INSTALL_DIR}/conda_install.sh && \
+bash ${INSTALL_DIR}/conda_install.sh -b -p ${INSTALL_DIR}/${CONDA} && \
+rm ${INSTALL_DIR}/conda_install.sh && \
+export PATH="${INSTALL_DIR}/${CONDA}/bin:${PATH}"
