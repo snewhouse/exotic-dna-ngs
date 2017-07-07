05f4e05ab94ef15b4af619be30cda4ca0860c70b
snewhouse <stephen.j.newhouse@gmail.com>
2017-07-07 15:29:35 +0100

    WIP: Docker and install tweaks, not tested

--- src/install_anaconda3.sh
+++ src/install_anaconda3.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 INSTALL_DIR="${1:-${HOME}}" # "${HOME}"
+
 readonly ANCONDA_URL="https://repo.continuum.io/archive" ## 4.4.0
 readonly CONDA="Anaconda3"
 readonly CONDA_VERSION="4.4.0" # 4.4.0
 readonly ARCH="Linux-x86_64" # MacOSXx86_64
+
 echo "[INFO: $(date)] Installing: ${CONDA}-${CONDA_VERSION}-${ARCH}"
 echo "[INFO: $(date)] Installation Directory: ${INSTALL_DIR}"
+
 CONDA_BASH_INSTALL="${CONDA}-${CONDA_VERSION}-${ARCH}.sh"
+
 # install
-wget ${MINICONDA_URL}/${CONDA_BASH_INSTALL} -O ${INSTALL_DIR}/conda_install.sh && \
+CMD="wget ${MINICONDA_URL}/${CONDA_BASH_INSTALL} -O ${INSTALL_DIR}/conda_install.sh && \
 bash ${INSTALL_DIR}/conda_install.sh -b -p ${INSTALL_DIR}/${CONDA} && \
-rm ${INSTALL_DIR}/conda_install.sh && \
+rm ${INSTALL_DIR}/conda_install.sh"
+
+echo "[INFO: $(date)] Running CMD: ${CMD}"
+
+${CMD}
+
 export PATH="${INSTALL_DIR}/${CONDA}/bin:${PATH}"
--- src/install_miniconda.sh
+++ src/install_miniconda.sh
@@ -4,11 +4,19 @@ readonly MINICONDA_URL="https://repo.continuum.io/miniconda" ## 4.3.21
 readonly CONDA="Miniconda3"
 readonly CONDA_VERSION="4.3.21" # "latest"; "4.3.21" # July 2017: latest
 readonly ARCH="Linux-x86_64" # MacOSXx86_64
+
 echo "[INFO: $(date)] Installing: ${CONDA}-${CONDA_VERSION}-${ARCH}"
 echo "[INFO: $(date)] Installation Directory: ${INSTALL_DIR}"
+
 CONDA_BASH_INSTALL="${CONDA}-${CONDA_VERSION}-${ARCH}.sh"
+
 # install
-wget ${MINICONDA_URL}/${CONDA_BASH_INSTALL} -O ${INSTALL_DIR}/conda_install.sh && \
+CMD="wget ${MINICONDA_URL}/${CONDA_BASH_INSTALL} -O ${INSTALL_DIR}/conda_install.sh && \
 bash ${INSTALL_DIR}/conda_install.sh -b -p ${INSTALL_DIR}/${CONDA} && \
-rm ${INSTALL_DIR}/conda_install.sh && \
+rm ${INSTALL_DIR}/conda_install.sh"
+
+echo "[INFO: $(date)] Running CMD: ${CMD}"
+
+${CMD}
+
 export PATH="${INSTALL_DIR}/${CONDA}/bin:${PATH}"
