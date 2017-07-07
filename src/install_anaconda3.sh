b7bd15feaf393a6cda374d4716e2a0ab409233ed
snewhouse <stephen.j.newhouse@gmail.com>
2017-07-07 15:01:25 +0100

    sep conda install scripts: start & Dockerfile placeholder

--- src/install_anaconda3.sh
+++ src/install_anaconda3.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-#!/usr/bin/env bash
+INSTALL_DIR="${1-HOME}" # "${HOME}"
 readonly ANCONDA_URL="https://repo.continuum.io/archive" ## 4.4.0
 readonly CONDA="Anaconda3"
 readonly CONDA_VERSION="4.4.0" # 4.4.0
 readonly ARCH="Linux-x86_64" # MacOSXx86_64
-INSTALL_DIR="${1-HOME}" # "${HOME}"
 echo "[INFO: $(date)] Installing: ${CONDA}-${CONDA_VERSION}-${ARCH}"
 echo "[INFO: $(date)] Installation Directory: ${INSTALL_DIR}"
 CONDA_BASH_INSTALL="${CONDA}-${CONDA_VERSION}-${ARCH}.sh"
--- src/install_miniconda.sh
+++ src/install_miniconda.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
+INSTALL_DIR="${1-HOME}" # "${HOME}"
 readonly MINICONDA_URL="https://repo.continuum.io/miniconda" ## 4.3.21
 readonly CONDA="Miniconda3"
 readonly CONDA_VERSION="4.3.21" # "latest"; "4.3.21" # July 2017: latest
 readonly ARCH="Linux-x86_64" # MacOSXx86_64
-INSTALL_DIR="${1-HOME}" # "${HOME}"
 echo "[INFO: $(date)] Installing: ${CONDA}-${CONDA_VERSION}-${ARCH}"
 echo "[INFO: $(date)] Installation Directory: ${INSTALL_DIR}"
 CONDA_BASH_INSTALL="${CONDA}-${CONDA_VERSION}-${ARCH}.sh"
